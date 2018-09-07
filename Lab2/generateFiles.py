5f0487a2ed35ead99cc4e28dea4836f75d98dc2c
Rafael Baldasso Audibert <32079912+rafaeelaudibert@users.noreply.github.com>
2018-09-06 22:50:07 -0300

    :tada: Adding professor files for Lab2

---
+++ Lab2/generateFiles.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+filesDir = "files/"
+if not os.path.exists(filesDir):
+    os.makedirs(filesDir)
+
+size = 100000
+maxNumber = 999999
+numFiles = 100
+beginNum = 0
+
+random.seed()
+
+for i in range(beginNum,beginNum+numFiles):
+    name = "file%02d.txt"%i 
+    print(name)
+    
+    file=open(filesDir+name,"w")
+    for l in range(size):
+        x = random.randint(0, maxNumber)
+        file.write("%06d "%x)
+        if l%20==19:
+            file.write("\n")
+
+
+
